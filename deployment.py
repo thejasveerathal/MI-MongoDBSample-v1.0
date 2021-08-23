70a937fa7fa2c94bbc6635c5dae93495706a18f8
thejasveerathal <32195119+thejasveerathal@users.noreply.github.com>
2021-08-23 06:32:34 -0700

    IP update

--- deployment.py
+++ deployment.py
@@ -1,7 +1,7 @@
 import deploymentlib
 from pathlib import Path
 
-nexusrepositoryurl = "http://172.18.0.2:8081/repository/micro-integrator/MI-MongoDBSample/MI-MongoDBSample-CAR/1.0.0/MI-MongoDBSample-CAR-1.0.0.zip"
+nexusrepositoryurl = "http://172.18.0.3:8081/repository/micro-integrator/MI-MongoDBSample/MI-MongoDBSample-CAR/1.0.0/MI-MongoDBSample-CAR-1.0.0.zip"
 nexusurlpath = Path(nexusrepositoryurl)
 artifactname = nexusurlpath.name
 
